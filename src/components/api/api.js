782a3c5db44bb87e732e62fe73d75260ca31cfee
James Efienemokwu <jefiene77@gmail.com>
2022-04-10 04:54:19 +0100

    update

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Style  from "./api.module.css";
+import search from '../../images/search.png'
 
 
 const Api = () => {
     const [data, setData] = useState([])
-    const [assetBal, setAssetBal] = useState([])
+    // const [assetBal, setAssetBal] = useState([])
     let arr = [];
     const getApi = async() =>{
         const fetchApi = await fetch("https://atm-web-291907.ue.r.appspot.com/balances/0x1A4b46696b2bB4794Eb3D4c26f1c55F9170fa4C5");
@@ -19,14 +20,9 @@ const Api = () => {
             rate: item.quote_rate
             })
         });
-
-        console.log(arr);
-        setAssetBal(arr);
-        setData(useApi);
-        console.log(arr);
+        // setAssetBal(arr);
+        setData(arr);
     }
-
-  console.log(data);
 
   function exactToken(amount) {
     return (amount / Math.pow(10, 18)).toFixed(2);
@@ -37,18 +33,36 @@ const Api = () => {
   },[])
 
   return (
-    <div className={Style.container}>Api
-      {
-        arr.map(
-          item =>{
-            return(<div>
-              {item.name}
-              console.log(item.name);
-            </div>)
-          }
-        )
-      }
-    </div>
+          <div className={Style.container}>
+              <div className={Style.searchbar}>
+                  <input type="search" name="" id="" placeholder='Search for DAO’s'/>
+                  <img src={search} alt="" />
+              </div>
+            <div className={Style.wrapper}>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Asset</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                {
+                    data.map((item, index) =>{
+                        return (
+                            <tbody key={index}>
+                                <tr>
+                                    <td>{item.name}</td>
+                                    <td>{item.rate}</td>
+                                    <td>{item.balance}</td>
+                                </tr>
+                            </tbody>
+                        )
+                    } )
+                }
+            </table>
+            </div>
+        </div>
   )
 }
 
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Header from '../../components/header/header'
 import Sidebar from '../../components/sidebar/sidebar'
 import Api from '../../components/api/api'
+import Styles from './dashboard.module.css'
 
 const Dashboard = () => {
   return (
     <div>
         <Header/>
-        <Sidebar/>
-        <Api/>
+        <div className={Styles.wrapper}>
+            <Sidebar/>
+            <Api/>
+        </div>
     </div>
   )
 }
