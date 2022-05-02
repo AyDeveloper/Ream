ff755b52e4f3a977a8a92e076bc2ececca41f1af
jefedcreator <jefiene77@gmail.com>
2022-05-02 10:29:09 +0100

    improved contract functionality

--- contracts/reamfactory.sol
+++ contracts/reamfactory.sol
@@ -5,11 +5,25 @@ import  "./ream.sol";
 
 contract ReamFactory {
 
+    address public admin;
+    constructor(address _admin) {
+        admin = admin;
+    }
+
     Ream[] private _Ream;
 
     event CreateReam(address _admin, address contractAddr);
+    event Send(uint amount, address to, string desc);
+    event Receive(uint amount, address from);
     mapping(address => bool) public userCreated;
     mapping(address => address) public userToReamAddr;
+    mapping(address=>uint) depositor;
+
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
 
     function createReamTreasury() public returns (address){
         if(userCreated[msg.sender] == true) return userToReamAddr[msg.sender]; 
@@ -17,6 +31,18 @@ contract ReamFactory {
         userToReamAddr[msg.sender] = address(ream);
         userCreated[msg.sender] = true;
         emit CreateReam(msg.sender, address(ream));
+    }
+
+    function sendFundsToReam(uint amount, address _to, string memory desc) public onlyAdmin {
+        (bool sent, ) = _to.call{value:amount}(abi.encodeWithSignature("sendFunds(uint amount, address _to, string memory desc)",amount,_to,desc));
+        require(sent, "Failed to send");
+        emit Send(amount, _to, desc);
+    }
+
+    function depositToReam(uint amount) public payable onlyAdmin{
+        depositor[msg.sender] += msg.value;
+        (bool sent, ) = userToReamAddr[msg.sender].call{value:amount}(abi.encodeWithSignature("deposit(amount)"));
+        emit Receive(msg.value, msg.sender);
     }
 
     function allReamTreasury() public view returns (Ream[] memory) {
